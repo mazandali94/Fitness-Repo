2b6829eb8670976a697f77888cdfebcbc3c53ba3
Mazan Dali <dalimazan94@gmail.com>
2021-09-19 21:53:25 +0300

    start2

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Expanded(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Text(
+                  "dsdsdsd",
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
