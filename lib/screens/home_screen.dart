086dcf224ce64fd88da5d98f3a6dbe1146a8bf9b
Mazan Dali <dalimazan94@gmail.com>
2021-09-20 18:16:41 +0300

    home screen-header exp

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,20 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Expanded(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                child: Text(
-                  "dsdsdsd",
+      body: SafeArea(
+        child: Column(
+          children: [
+            Stack(
+              clipBehavior: Clip.none,
+              fit: StackFit.loose,
+              children: [
+                Expanded(
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    color: const Color(0xFFF58591),
+                  ),
                 ),
-              ),
-            ],
-          ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    // mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(child: Text("Exercises")),
+                      Row(
+                        children: [
+                          Icon(Icons.search),
+                          Icon(Icons.more_horiz),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                ImageSlideshow(
+                  width: double.infinity,
+                  height: 200,
+                  initialPage: 0,
+                  indicatorColor: Colors.blue,
+                  indicatorBackgroundColor: Colors.grey,
+                  onPageChanged: (value) {
+                    debugPrint('Page changed: $value');
+                  },
+                  autoPlayInterval: 3000,
+                  isLoop: true,
+                  children: [
+                    Image.asset(
+                      'assets/images/image_1.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                    Image.asset(
+                      'assets/images/image_2.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                    Image.asset(
+                      'assets/images/image_3.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
